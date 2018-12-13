e9df4cdc3ec59f0d327c0c5051fc790a9351cc47
Lecu Radu <lecu.radu@gmail.com>
2018-12-14 00:57:47 +0200

    create MapBuilder

---
+++ src/main/java/ro/rasel/collections/MapBuilder.java
@@ -0,0 +1,68 @@
+package ro.rasel.collections;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class MapBuilder<K, V> {
+    private final Map<K, V> map;
+
+    private MapBuilder(Supplier<Map<K, V>> supplier) {
+        this.map = supplier.get();
+    }
+
+    public static <K, V> MapBuilder<K, V> ofMap() {
+        return new MapBuilder<>(HashMap::new);
+    }
+
+    public static <K, V> MapBuilder<K, V> ofOrderedMap() {
+        return new MapBuilder<>(LinkedHashMap::new);
+    }
+
+    public MapBuilder<K, V> put(K k, V v) {
+        map.put(k, v);
+        return this;
+    }
+
+    public MapBuilder<K, V> putAll(Map<? extends K, ? extends V> map) {
+        this.map.putAll(map);
+        return this;
+    }
+
+    public MapBuilder<K, V> remove(K k) {
+        map.remove(k);
+        return this;
+    }
+
+    /**
+     * While {@link MapBuilder} has the purpose to help creating a new {@link Map} or any subtype by using the builder
+     * pattern the client of the method will have to actually create it himself by providing a function that should be
+     * used for bot creation of the new {@link Map} and copying the content.
+     * This allows for both creation of any type of {@link Map} and a way to create an immutable {@link Map}.<br>
+     *
+     * <pre> {@code mapBuilder.build(HashMap::new);
+     *  mapBuilder.build(m -> Collections.unmodifiableNavigableMap(new TreeMap<>(m))); } </pre>
+     * <p>
+     * While the client can use the identity function this is not normally a good approach from the perspective of builder parent.
+     *
+     * <pre> {@code mapBuilder.build(Function.identity()); } </pre>
+     * <p>
+     * From the perspective of correctness of the code this is does not pose any risk as long as an unordered or
+     * ordered map is needed, the build method is called only once and none of the modify methods are called for the
+     * builder after that. <br>
+     * In addition to that the builder can be used to build any Object based on that map like in the following example:
+     *
+     * <pre> {@code Stream<Integer> stream = mapBuilder.build(map -> map.keySet().stream());
+     *  stream.map(Object::toString).collect(Collectors.joining(", "));} </pre>
+     *
+     * @param function
+     * @param <R>
+     * @return
+     */
+    public <S> S build(Function<Map<K, V>, ? extends S> function) {
+        return function.apply(Collections.unmodifiableMap(map));
+    }
+}
---
+++ src/test/java/ro/rasel/collections/MapBuilderTest.java
@@ -0,0 +1,89 @@
+package ro.rasel.collections;
+
+import org.junit.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class MapBuilderTest {
+
+    @Test
+    public void mapTest() {
+        MapBuilder<Integer, String> mapBuilder =
+                MapBuilder.<Integer, String>ofMap().put(2, "2").put(1, "One").put(2, "Two").put(3, "Three");
+
+        Map<Integer, String> hashMap = mapBuilder.build(HashMap::new);
+        Map<Integer, String> linkedHashMap = mapBuilder.build(LinkedHashMap::new);
+        Map<Integer, String> treeMap = mapBuilder.build(TreeMap::new);
+        Map<Integer, String> immutableMap = mapBuilder.build(m -> Collections.unmodifiableMap(new HashMap<>(m)));
+        Map<Integer, String> immutableNavigableMap =
+                mapBuilder.build(m -> Collections.unmodifiableNavigableMap(new TreeMap<>(m)));
+
+        assertMapContentAndType(hashMap, HashMap.class);
+        assertMapContentAndType(linkedHashMap, LinkedHashMap.class);
+        assertMapContentAndType(treeMap, TreeMap.class);
+        assertMapContentAndType(immutableMap, Map.class);
+        assertMapContentAndType(immutableNavigableMap, NavigableMap.class);
+    }
+
+    @Test
+    public void orderedMapTest() {
+        MapBuilder<Integer, String> orderedMapBuilder =
+                MapBuilder.<Integer, String>ofOrderedMap()
+                        .put(2, "2")
+                        .put(1, "One")
+                        .put(5, "Five")
+                        .put(2, "Two")
+                        .put(4, "Four")
+                        .put(3, "Three");
+
+        Map<Integer, String> linkedHashMap = orderedMapBuilder.build(LinkedHashMap::new);
+        ArrayList<Integer> integers = new ArrayList<>(linkedHashMap.keySet());
+        assertThat(integers.get(0), is(2));
+        assertThat(integers.get(1), is(1));
+        assertThat(integers.get(2), is(5));
+        assertThat(integers.get(3), is(4));
+        assertThat(integers.get(4), is(3));
+
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testImmutable() {
+        MapBuilder<Integer, String> mapBuilder =
+                MapBuilder.<Integer, String>ofMap().put(2, "2").put(1, "One").put(2, "Two").put(3, "Three");
+        Map<Integer, String> immutableMap = mapBuilder.build(m -> Collections.unmodifiableMap(new HashMap<>(m)));
+        immutableMap.put(4, "4");
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testImmutableMapProvidedInFunction() {
+        MapBuilder<Integer, String> mapBuilder =
+                MapBuilder.<Integer, String>ofMap().put(2, "2").put(1, "One").put(2, "Two").put(3, "Three");
+        mapBuilder.build(m -> {
+            m.put(4, "4");
+            return Collections.unmodifiableMap(new HashMap<>(m));
+        });
+    }
+
+    @Test
+    public void testBuilderForNonMapObjects() {
+        MapBuilder<Integer, String> mapBuilder =
+                MapBuilder.<Integer, String>ofOrderedMap().put(2, "2").put(1, "One").put(2, "Two").put(3, "Three");
+        String join =
+                mapBuilder.build(map -> map.keySet().stream()).map(Object::toString).collect(Collectors.joining(", "));
+        assertThat(join, is("2, 1, 3"));
+    }
+
+    private static <R extends Map<?, ?>> void assertMapContentAndType(R map, Class<? extends R> clazz) {
+        assertThat(map.get(1), is("One"));
+        assertThat(map.get(2), is("Two"));
+        assertThat(map.get(3), is("Three"));
+        assertThat(map.size(), is(3));
+        assertTrue(clazz.isAssignableFrom(map.getClass()));
+    }
+
+}
